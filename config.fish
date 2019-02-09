4d19b1d2f29f800df3bada2f4216a1b6019c8004
Sasasu <su@sasasu.me>
2019-02-09 19:08:26 +0800

    s

--- config.fish
+++ config.fish
@@ -1,16 +1,3 @@
 set -gx fish_greeting ""
-    function ia
-	set platform (uname | grep Darwin)
-	if [ $platform ]
-		echo "set mac PATH"
-		set -gx PATH /Users/$USER/anaconda2/bin $PATH
-	else
-		echo "set linux PATH"
-		set -gx PATH /opt/anaconda/bin $PATH
-	end
-	source (conda info --root)/etc/fish/conf.d/conda.fish
-end
-
-set -gx PATH ~/.cargo/bin $PATH
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 export GPG_TTY=(tty)
