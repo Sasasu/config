1bcebfc753d85937122fb5379d15bcccbd7fb60c
Sasasu <su@sasasu.me>
2019-07-27 18:52:43 +0800

    tmux

--- config.fish
+++ config.fish
@@ -3,3 +3,16 @@ set -gx fish_greeting ""
 export PATH="$PATH:~/.gem/ruby/2.6.0/bin/:~/.local/bin/"
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 export GPG_TTY=(tty)
+
+alias ls exa
+
+# TMUX
+if which tmux >/dev/null 2>&1
+    # if no session is started, start a new session
+    test -z $TMUX && exec tmux
+
+    # when quitting tmux, try to attach
+    while test -z $TMUX
+        tmux attach || break
+    end
+end
