2060f0af1cc8d37097f43518b7497682dd520804
Sasasu <i@sasa.su>
2024-03-13 04:45:15 -0400

    s

--- config.fish
+++ config.fish
@@ -1,14 +1,23 @@
 set -gx SPACEFISH_KUBECONTEXT_SHOW false
 set -gx fish_greeting ""
-export PATH="$PATH:~/.local/bin/"
+
+if test "$(uname -o)" = "Darwin"
+    export PATH="/opt/homebrew/bin:$PATH"
+    export PATH="/opt/homebrew/sbin:$PATH"
+    export PATH="/usr/local/bin:$PATH"
+    export PATH="$HOME/.cargo/bin:$PATH"
+end
+
+export PATH="$PATH:$HOME/.local/bin/"
+
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 export GPG_TTY=(tty)
+gpgconf --launch gpg-agent
 
 export LC_ALL=en_US.UTF-8
 export LANGUAGE=en_US:en_US
 
 alias ls exa
-#alias tig gitui
 alias gitc "git commit -s"
 alias make "make -j8"
 alias ag "rg"
@@ -30,5 +39,7 @@ end
 alias pp "ps aux | grep postgres | grep -v grep | grep -P '\d'"
 alias ppc "ps aux | grep postgres | grep -v grep | grep con | grep -P '\d'"
 
-export JAVA_HOME=/usr/lib/jvm/default-runtime
+#export JAVA_HOME=/usr/lib/jvm/default-runtime
+starship init fish | source
+
 export EDITOR=/usr/bin/nvim
